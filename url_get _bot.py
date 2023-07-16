c9573bfc93336eda0a75010d977c82a3ce77217d
fanbook-wangdage <124357765+fanbook-wangdage@users.noreply.github.com>
2023-07-16 16:14:51 +0800

    更新了注释

--- url_get _bot.py
+++ url_get _bot.py
@@ -12,16 +12,16 @@ from pygments.lexers import JsonLexer
 from pygments.formatters import TerminalFormatter
 
 
-null=None
+null=None#为了替换返回中的信息，测试用
 false=False
-lingpai='0f2de7ac66727cd9fcec1ee43559c561f6abf3f1e202c5a06c2ae4a3f6cf94ab795fbfd311a18ad1ff314388d1c'
-si=['目前只支持图片、视频、文件，支持类型以外可能会导致获取错误','体验机器人、网站、软件，欢迎加入服务器：https://fanbook.mobi/LmgLJF3N ','如遇到消息没有正常显示或者没有反应，可稍等重试\n此服务仅在中午以后，晚上10点以前可用，如果仍然不能使用，请私信:${@!389320179948986368}','如获取不成功，可尝试更换文件','手动获取图片链接:https://fanbookwdg3.zmidc.eu.org/']
+lingpai='0f2de7ac66727cd9fcec1ee43559c561f6abf3f1e202c5ab795fbfd311a18ad1ff314388d1c'#换为你自己的机器人令牌
+si=['目前只支持图片、视频、文件，支持类型以外可能会导致获取错误','体验机器人、网站、软件，欢迎加入服务器：https://fanbook.mobi/LmgLJF3N ','如遇到消息没有正常显示或者没有反应，可稍等重试\n此服务仅在中午以后，晚上10点以前可用，如果仍然不能使用，请私信:${@!389320179948986368}','如获取不成功，可尝试更换文件','手动获取图片链接:https://fanbookwdg3.zmidc.eu.org/']#随机提示，可自己改，记得在消息文本那里改随机数
 data_queue = queue.Queue()
-sc=0
+sc=0#统计变量
 cwc=0
 
 
-def colorize_json(smg2,pcolor=''):
+def colorize_json(smg2,pcolor=''):#JSON高亮，如果你的终端软件显示不正常，可直接改为print(json_data)
     json_data=smg2
     try:
         parsed_json = json.loads(json_data)  # 解析JSON数据
@@ -39,8 +39,8 @@ def on_message(ws, message):
     colorize_json(message)
     global sc
     global cwc
-    # 在这里添加你希望执行的操作
-    if len(message) > 100:
+    # 在这里添加收到消息后你希望执行的操作
+    if len(message) > 100:#筛选有效信息
         try:
             data=message
             data = json.loads(data)
@@ -52,7 +52,7 @@ def on_message(ws, message):
                         headers = {'content-type':"application/json;charset=utf-8"}
                         jsonfile=json.dumps({
                         "chat_id":int(data['data']['channel_id']),
-                        "text": '获取成功，图片/视频链接：\n'+content["url"]+'\n欢迎下次使用\n提示：'+si[random.randint(0,4)]
+                        "text": '获取成功，图片/视频链接：\n'+content["url"]+'\n欢迎下次使用\n提示：'+si[random.randint(0,4)]#这里的随机数需要按照你的提示数量修改
                         })
                         postreturn=requests.post(url,data=jsonfile,headers=headers)
                         colorize_json(postreturn.text)
@@ -86,7 +86,7 @@ def on_message(ws, message):
                                 except Exception as e:
                                     print(traceback.format_exc())
             else:
-                if '${@!448828939389894656}' in data["data"]["content"]:
+                if '${@!448828939389894656}' in data["data"]["content"]:#也可以增加在频道使用功能
                     url='https://a1.fanbook.mobi/api/bot/'+lingpai+'/sendMessage'
                     headers = {'content-type':"application/json;charset=utf-8"}
                     jsonfile=json.dumps({
@@ -111,7 +111,7 @@ def on_open(ws):
     # 连接建立时的操作
     print("连接已建立")
     # 发送心跳包
-    def send_ping():
+    def send_ping():#此处不太正常，忽略
         print('发送：{"type":"ping"}')
         ws.send('{"type":"ping"}')
 
@@ -125,7 +125,7 @@ def on_open(ws):
         ws.send_ping()
         websocket._get_connection().sock.settimeout(70)
         ws.send('{"type":"ping"}')
-    websocket._get_connection().run_forever(ping_interval=25, ping_payload='{"type":"ping"}', ping_schedule=schedule_ping)
+    websocket._get_connection().run_forever(ping_interval=25, ping_payload='{"type":"ping"}', ping_schedule=schedule_ping)#定时发送有问题，忽略，已在第二线程中实现
 
 # 替换成用户输入的BOT令牌
 lingpai = lingpai
@@ -136,7 +136,7 @@ response = requests.get(url)
 data = response.json()
 
 def send_data_thread():
-    while True:
+    while True:#多线程自动发送心跳包，互不干扰
         # 在这里编写需要发送的数据
         time.sleep(25)
         ws.send('{"type":"ping"}')
